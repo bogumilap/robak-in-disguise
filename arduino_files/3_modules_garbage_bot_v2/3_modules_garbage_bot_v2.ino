dfba27fbc12fead4f9017885a7bd605da9f48128
tunczyk101 <jolka2102@gmail.com>
2023-03-30 10:27:49 +0200

    3 modules v2

---
+++ arduino_files/3_modules_garbage_bot_v2/3_modules_garbage_bot_v2.ino
@@ -0,0 +1,43 @@
+#include <Servo.h>
+
+// create servo objects to control servos
+Servo servo8;
+Servo servo9;  
+
+int angle = 45;
+
+
+void setup() {
+  // attaches servoes on pins to the servo objects
+  servo8.attach(8);
+  servo9.attach(9); 
+}
+
+void loop() {
+  servo8.write(90);  // tell servo to go to given position
+  servo9.write(90);
+
+  double ratio8 = 2;  // 90 / 45
+
+  if (angle < 90)
+  {
+    for (int step=0; step<angle; step++) {  // servo8 is going up
+      servo8.write(max(90 - ratio8 * step, 0));
+      servo9.write(90 - step);
+      delay(25);  // waits 25ms for the servos to reach positions
+    }
+    for (int step=0; step<angle; step++) {
+      servo9.write(angle + 3 * step);
+      delay(25);
+    }
+    for (int step=0; step<angle; step++) {
+      servo8.write(3 * step);
+      delay(25);
+    }
+    for (int step=0; step<angle; step++) {
+      servo8.write(3 * angle - step);
+      servo9.write(180 - 2 * step);
+      delay(25);
+    }
+  }
+}
