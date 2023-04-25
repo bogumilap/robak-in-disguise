bb107d621ec623b90ff9eb26926600560cc66b8a
tunczyk101 <jolka2102@gmail.com>
2023-04-25 11:43:45 +0200

    add servo

--- arduino_files/fold/fold.ino
+++ arduino_files/fold/fold.ino
@@ -3,7 +3,7 @@
 // create servo objects to control servos
 Servo servo8;
 Servo servo9;
-Servo servo10; 
+Servo servo10;  
 
 // int angle = 45;
 
@@ -12,13 +12,13 @@ void setup() {
   // attaches servoes on pins to the servo objects
   servo8.attach(8);
   servo9.attach(9);
-  servo10.attach(10);
+  servo10.attach(10); 
 }
 
 void loop() {
-  servo8.write(90);  // tell servo to go to given position
-  servo9.write(90);
-  servo10.write(90);
+  servo8.write(180);  // tell servo to go to given position
+  servo9.write(0);
+  servo10.write(180);
 
   // double ratio8 = 2;  // 90 / 45
 
