ebd09365c0d43441f8f8e8fd25f190e8f547e4de
bogumilap <bogumila.papiernik@gmail.com>
2023-05-09 12:14:04 +0200

    create potentially final version of movement

--- arduino_files/4_modules_garbage_bot_v2/4_modules_garbage_bot_v2.ino
+++ arduino_files/4_modules_garbage_bot_v2/4_modules_garbage_bot_v2.ino
@@ -14,33 +14,49 @@ void setup() {
   servo9.attach(9); 
   servo10.attach(10);
 }
-// 90, 90, 90
-// 0, 0, 90
-// 0, 45, 90
-// 0, 45, 135
-// 90, 90, 135
-// 90, 90, 90
+
 void loop() {
+  // 90, 90, 90
   servo8.write(90);  // tell servo to go to given position
   servo9.write(90);
   servo10.write(90);
 
   double ratio8 = 2;  // 90 / 45
-  int delay_value = 30;
+  int delay_value = 15;
 
   if (angle < 90)
   {
+    // 0, 0, 90
     for (int step=0; step<angle; step++) {
-      servo8.write(min(90 - ratio8 * step, 0));
-      servo9.write(min(90 - ratio8 * step, 0));
+      servo8.write(max(90 - ratio8 * step, 0));
+      // servo9.write(90 - step);
+      servo9.write(max(90 - ratio8 * step, 0));
       delay(delay_value);
     }
+
+    // 0, 45, 90
     for (int step=0; step<angle; step++) {
-      servo9.write(max(step, 45));
+      servo9.write(step);
       delay(delay_value);
     }
+
+    // 0, 45, 135
     for (int step=0; step<angle; step++) {
-      servo10.write(max(90 + step, 135));
+      servo10.write(min(90 + step, 135));
+        delay(delay_value);
+    }
+
+    // 90, 90, 135
+    for (int step=0; step<angle; step++) {
+      servo8.write(ratio8 * step);
+      servo9.write(45 + step);
+      delay(delay_value);
+    }
+    
+    // 90, 90, 90
+    for (int step=0; step<angle; step++) {
+      servo10.write(135 - step);
+      delay(delay_value);
     }
   }
 }
