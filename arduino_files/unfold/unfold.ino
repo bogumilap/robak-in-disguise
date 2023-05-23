527813af8e9d5816212a04b6d9181b55e9a93b57
bogumilap <bogumila.papiernik@gmail.com>
2023-05-23 12:02:31 +0200

    improve fold, create unfold

--- arduino_files/unfold/unfold.ino
+++ arduino_files/unfold/unfold.ino
@@ -4,7 +4,7 @@
 Servo servo8;
 Servo servo9;
 Servo servo10;  
-bool folded = false;
+bool unfolded = false;
 
 void setup() {
   servo8.attach(8);
@@ -13,16 +13,16 @@ void setup() {
 }
 
 void loop() {
-  if (! folded) {
-    for (int step=90; step<180; step++) {
+  if (! unfolded) {
+    for (int step=0; step<90; step++) {
+      servo9.write(step);
+      delay(30);
+    }
+    for (int step=180; step>90; step--) {
       servo8.write(step);
       servo10.write(step);
       delay(30);
     }
-    for (int step=90; step>0; step--) {
-      servo9.write(step);
-      delay(30);
-    }
-    folded = true;
+    unfolded = true;
   }
 }
