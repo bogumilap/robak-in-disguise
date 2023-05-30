14823f9a34410d454d05886cb99f97abe8950bb8
bogumilap <bogumila.papiernik@gmail.com>
2023-05-30 12:12:46 +0200

    add dancing

--- robak/robak.ino
+++ robak/robak.ino
@@ -1,6 +1,6 @@
 int MIN_US = 544;
 int MAX_US = 2400;
-int DELAY_VALUE = 10;
+int DELAY_VALUE = 5;
 bool FOLDED = false;
 bool CRAWLING = false;
 
@@ -59,6 +59,12 @@ void processInput(String inputString) {
     } else {
       stop();
     }
+  }
+  if (isInputEqual(inputString, "dance")) {
+    if (FOLDED) {
+      unfold();
+    }
+    dance();
   }
 }
 
@@ -119,6 +125,7 @@ void fold() {
   if (! FOLDED) {
     for (int step=90; step<180; step++) {
       setAngle(step, 8);
+      // setAngle(step, 10);
       delay(DELAY_VALUE);
     }
     for (int step=90; step>0; step--) {
@@ -145,5 +152,176 @@ void unfold() {
       delay(DELAY_VALUE);
     }
     FOLDED = false;
+  } else {
+    stop();
   }
 } 
+
+void dance() {
+  stop();
+  int base_angle = 90;
+  int angle = 45;
+  // one second - arm 1
+  for (int step=0; step<50; step++) {  // servo8 is going up
+    setAngle(base_angle + step, 8);
+    delay(10); 
+  }
+  for (int step=49; step>=0; step--) {  // servo8 is going up
+    setAngle(base_angle + step, 8);
+    delay(10); 
+  }
+
+  // 1 s - arm 2
+  for (int step=0; step<50; step++) {  // servo8 is going up
+    setAngle(base_angle + step, 10);
+    delay(10); 
+  }
+  for (int step=49; step>=0; step--) {  // servo8 is going up
+    setAngle(base_angle + step, 10);
+    delay(10); 
+  }
+
+  // 1 s - /\/\ ...
+  for (int step=0; step<angle; step++) {  // servo8 is going up
+    setAngle(base_angle - 2 * step, 8);
+    setAngle(base_angle - 2 * step, 9);
+    setAngle(base_angle - 2 * step, 10);
+    delay(11); 
+  }
+  delay(5);
+  for (int step=angle-1; step>=0; step--) {  // servo8 is going up
+    setAngle(base_angle - 2 * step, 8);
+    setAngle(base_angle - 2 * step, 9);
+    setAngle(base_angle - 2 * step, 10);
+    delay(11); 
+  }
+  delay(5);
+
+  // 1 s - \_/
+  for (int step=0; step<50; step++) {  // servo8 is going up
+    setAngle(base_angle + step, 8);
+    setAngle(base_angle + step, 10);
+    delay(10); 
+  }
+  for (int step=49; step>=0; step--) {  // servo8 is going up
+    setAngle(base_angle + step, 8);
+    setAngle(base_angle + step, 10);
+    delay(10); 
+  }
+
+  // 4 s - arm 1, 2 up, down
+  for (int step=0; step<50; step++) {  // servo8 is going up
+    setAngle(base_angle + step, 8);
+    delay(20); 
+  }
+  for (int step=0; step<50; step++) {  // servo8 is going up
+    setAngle(base_angle + step, 10);
+    delay(20); 
+  }
+  for (int step=49; step>=0; step--) {  // servo8 is going up
+    setAngle(base_angle + step, 10);
+    delay(20); 
+  }
+  for (int step=49; step>=0; step--) {  // servo8 is going up
+    setAngle(base_angle + step, 8);
+    delay(20); 
+  }
+
+  for(int g=0; g<2; g++) {
+    // sum = 2s 
+    // 0.5s - /\_
+    for (int step=0; step<angle; step++) {  // servo8 is going up
+      setAngle(base_angle + 2 * step, 8);
+      setAngle(base_angle + step, 9);
+      delay(11); 
+    }
+    delay(5);
+
+    // 0.5s - /\/\ ...
+    for (int step=0; step<angle; step++) {  // servo8 is going up
+      setAngle(base_angle + 2 * step, 10);
+      setAngle(base_angle + angle + step, 9);
+      delay(11); 
+    }
+    delay(5);
+
+    // 0.5s - /\_
+    for (int step=angle-1; step<=0; step--) {  // servo8 is going up
+      setAngle(base_angle - 2 * step, 8);
+      setAngle(base_angle + angle - step, 9);
+      delay(11); 
+    }
+    delay(5);
+    // 0.5s - ____
+    for (int step=angle-1; step<=0; step--) {  // servo8 is going up
+      setAngle(base_angle - 2 * step, 8);
+      setAngle(base_angle - step, 9);
+      delay(11); 
+    }
+    delay(5);
+
+
+    // fold - 4s
+    for (int step=0; step<angle; step++) {
+      setAngle(base_angle + 2 * step, 8);
+      setAngle(base_angle + 2 * step, 10);
+      delay(44); 
+    }
+    delay(20);
+    for (int step=0; step<angle; step++) {
+      setAngle(base_angle - 2 * step, 9);
+      delay(44); 
+    }
+    delay(20);
+
+    // unfold - 4s
+    for (int step=0; step<angle; step++) {
+      setAngle(base_angle + 2 * (angle - step - 1), 9);
+      delay(44); 
+    }
+    delay(20);
+    for (int step=0; step<angle; step++) {
+      setAngle(base_angle - 2 * (angle - step - 1), 8);
+      setAngle(base_angle - 2 * (angle - step - 1), 10);
+      delay(44); 
+    }
+    delay(20);
+
+    // fala - 6 s
+    // /\_ - 1s
+    for (int step=0; step<angle; step++) {  // servo8 is going up
+      setAngle(base_angle + 2 * step, 8);
+      setAngle(base_angle + step, 9);
+      delay(22); 
+    }
+    delay(10);
+    for(int i=0; i<2; i++) { // loop - 2s 
+    // _/\ - 1 s 
+      for (int step=0; step<angle; step++) {  // servo8 is going up
+        setAngle(base_angle + 2 * step, 10);
+        setAngle(base_angle + 2 * (angle - step - 1), 8);
+        delay(22); 
+      }
+      delay(10);
+      for (int step=0; step<angle; step++) {  // servo8 is going up
+        setAngle(base_angle + 2 * step, 8);
+        setAngle(base_angle + 2 * (angle - step - 1), 10);
+        delay(22); 
+      }
+      delay(10);
+    }
+    for (int step=0; step<angle; step++) {  // servo8 is going up
+      setAngle(base_angle + 2 * (angle - step - 1), 8);
+      setAngle(base_angle + (angle - step - 1), 9);
+      delay(22); 
+    }
+    delay(10);
+  }
+
+  for (int step=0; step<angle; step++) {  // servo8 is going up
+    setAngle(base_angle - 2 * step, 8);
+    setAngle(base_angle - min(step, 30), 9);
+    setAngle(base_angle - min(step, 15), 10);
+    delay(11); 
+  }
+}
